e3f03a453aea0a9644aa6af588f1cf097003d7f4
InfSecEng <ilyas.salimov.07@gmail.com>
2018-03-17 18:08:27 +0500

    redone in functional program

--- SortingAlgorithms/BubbleSort.py
+++ SortingAlgorithms/BubbleSort.py
@@ -2,14 +2,28 @@
 # Bubble sort algorithm.
 # ---------------------
 
-from random import randint                  # Importing randint item in random module.
-n = 10                                      # Amount items in list.
-a = [randint(0, n) for i in range(n)]       # Filling list of randoms numbers.
-for i in range(0, n - 1):                   # Passing through in list (n - 1) times.
-	f = 0                                   # Mark for break if list sorted.
-	for j in range(0, n - 1):               # Passing through in list from 0 to (n - 1).
-		if a[j] > a[j + 1]:                 # Comparison a[j] and a[j + 1].
-			a[j], a[j + 1] = a[j + 1], a[j] # Swap items.
-			f = 1                           # Mark for continued passing.
-	if f == 0:                              # If list sorted then
-		break                               # - break passing.
+def bubblesort(a, n):
+	for i in range(0, n - 1):                   # Passing through in list (n - 1) times.
+		f = 0                                   # Mark for break if list sorted.
+		for j in range(0, n - 1):               # Passing through in list from 0 to (n - 1).
+			if a[j] > a[j + 1]:                 # Comparison a[j] and a[j + 1].
+				a[j], a[j + 1] = a[j + 1], a[j] # Swap items.
+				f = 1                           # Mark for continued passing.
+		print(" ", [i + 1], "-->", a)           # Printing sorting status.
+		if f == 0:                              # If list sorted then
+			break                               # - break passing.
+	return a, i + 1                             # Return sorted list and number passages.
+
+def visualization():
+	from random import randint                  # Importing randint item in random module.
+	n = 10                                      # Amount items in list.
+	a = [randint(0, n) for i in range(n)]       # Filling list of randoms numbers.
+	print("Initial list:", a)                   # Printing initial list.
+	print("Visualization of algorithm work.")   # Printing decription.
+	a, i = bubblesort(a, n)                     # Sorting list.
+	print("Final list:", a)                     # Printing final list.
+	print("Total numbers of passages:", i)      # Printing numbers of passages.
+
+import timeit
+elapsed_time = timeit.timeit(visualization, number = 1) # Start program and counting elapsed time.
+print("Elapsed time: ", round(elapsed_time, 3), "sec.") # Printing elapsed time.
--- SortingAlgorithms/CocktailSort.py
+++ SortingAlgorithms/CocktailSort.py
@@ -2,16 +2,30 @@
 # Cocktail sort algortihm.
 # -----------------------
 
-from random import randint                  # Importing randint item in random module.
-n = 10                                      # Amount items in list.
-a = [randint(0, n) for i in range(n)]       # Filling list of randoms numbers.
-l, r = 0, n - 1                             # Create start and finish position.
-while l <= r:                               # Passing through in list.
-	for i in range(l, r, +1):               # Passing through from start to finish.
-		if a[i] > a[i + 1]:                 # If a[i] more a[i + 1] then
-			a[i], a[i + 1] = a[i + 1], a[i] # - swap a[i] and a[i + 1].
-	r -= 1                                  # Decrement r because value a last item the biggest.
-	for i in range(r, l, -1):               # Passing through from finish to start.
-		if a[i - 1] > a[i]:                 # If a[i - 1] more a[i] then
-			a[i], a[i - 1] = a[i - 1], a[i] # - swap a[i] and a[i - 1].
-	l += 1                                  # Increment l because value a first item the smallest.
+def cocktail(a, n):
+	l, r = 0, n - 1                             # Create start and finish position.
+	while l <= r:                               # Passing through in list.
+		for i in range(l, r, +1):               # Passing through from start to finish.
+			if a[i] > a[i + 1]:                 # If a[i] more a[i + 1] then
+				a[i], a[i + 1] = a[i + 1], a[i] # - swap a[i] and a[i + 1].
+		r -= 1                                  # Decrement r because value a last item the biggest.
+		for i in range(r, l, -1):               # Passing through from finish to start.
+			if a[i - 1] > a[i]:                 # If a[i - 1] more a[i] then
+				a[i], a[i - 1] = a[i - 1], a[i] # - swap a[i] and a[i - 1].
+		l += 1                                  # Increment l because value a first item the smallest.
+		print(" ", [l], "-->", a)               # Printing sorting status.
+	return a, l                                 # Return sorted list and number passages.
+
+def visualization():
+	from random import randint                  # Importing randint item in random module.
+	n = 10                                      # Amount items in list.
+	a = [randint(0, n) for i in range(n)]       # Filling list of randoms numbers.
+	print("Initial list:", a)                   # Printing initial list.
+	print("Visualization of algorithm work.")   # Printing decription.
+	a, i = cocktail(a, n)                       # Sorting list.
+	print("Final list:", a)                     # Printing final list.
+	print("Total numbers of passages:", i)      # Printing numbers of passages.
+
+import timeit
+elapsed_time = timeit.timeit(visualization, number = 1) # Start program and counting elapsed time.
+print("Elapsed time: ", round(elapsed_time, 3), "sec.") # Printing elapsed time.
