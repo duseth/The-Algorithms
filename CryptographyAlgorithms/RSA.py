c905dda53008ecc6bc32895d028dc595fdf89511
InfSecEng <ilyas.salimov.07@gmail.com>
2018-03-31 20:34:14 +0500

    redone to functional program with three modes

--- CryptographyAlgorithms/RSA.py
+++ CryptographyAlgorithms/RSA.py
@@ -27,84 +27,102 @@ def creation_keys():
 		d += 1                                       # Increment d for passing.
 	return (e, n), (d, n)                            # Return open and close keys.
 
-def encoding(text, open_key):
+def encoding():
 	"""Functionn for encoding text with RSA algorithm."""
+	global text, open_key, cipher_list
 	cipher_list = []                             # Creating cipher_list list for encoded chars.
 	e, n = open_key                              # Unpacking open_key tuple in e and n variables.
 	for letter in text:                          # Passing through from text.
 		cipher_list.append(ord(letter) ** e % n) # Append encoded char in cipher_list.
-	return cipher_list                           # Return encoded text as list.
 
-def decoding(cipher_list, close_key):
+def decoding():
 	"""Functionn for decoding text with RSA algorithm."""
+	global cipher_list, close_key, text
 	text = ""                           # Creating text string variable for decoded chars.
 	d, n = close_key                    # Unpacking close_key tuple in d and n variables.
 	for item in cipher_list:            # Passing through from cipher_list.
 		text += chr(int(item) ** d % n) # Concatenation decoded char to text.
-	return text                         # Return decoded text.
 
 # ============================
 # User and program interaction.
 # ============================
 
-def user_input():
-	"""Function to take text from the user and create keys."""
-	try:                                                                         # Creating exception constructor.
-		text = str(input("[+] Enter your text for encoding: "))                  # Input user text.
-		check = text[0]                                                          # Checking for an empty variable.
-	except:                                                                      # Exception handling.
-		print("[x] Incorrect input!")                                            # Printing about empty variable.
-		raise SystemExit                                                         # Exit from program.
-	open_key, close_key = creation_keys()                                        # Unpacking the tuples returned by the function.
-	print("  [!] Your open key: [{},{}].".format(open_key[0], open_key[1]))      # Printing open key.
-	print("  [!] Your close key: [{},{}].\n".format(close_key[0], close_key[1])) # Printing close key.
-	return text, open_key, close_key                                             # Return user data.
+def demonstration_mode():
+	global text, open_key, close_key, cipher_list
+	import timeit
+	print("\n » Demonstration mode activated! « ¯\_(ツ)_/¯")
+	try:
+		text = str(input("[+] Enter your text - "))
+		check = text[0]
+	except:
+		print("[x] Invalid input!")
+		raise SystemExit
+	open_key, close_key = creation_keys()
+	print("[~] Your open key - [{},{}].".format(open_key[0], open_key[1]))
+	print("[~] Your close key - [{},{}].".format(close_key[0], close_key[1]))
+	encoding_performance = "%.5f" % timeit.timeit(encoding, number = 1)
+	print("\n »» The result of encoding by RSA algorithm. ««")
+	print(cipher_list)
+	text = ""
+	decoding_performance = "%.5f" % timeit.timeit(decoding, number = 1)
+	print("\n »» The result of decoding by RSA algorithm. ««")
+	print(text)
+	print("\n[~] RSA algorithm encoding performance - {} sec.".format(encoding_performance))
+	print("[~] RSA algorithm decoding performance - {} sec.".format(decoding_performance))
 
-def run_encode():
-	"""Function for run encoding."""
-	global cipher_list, text, open_key               # Identify global variables.
-	print(" RSA algorithm encoding result showing.") # Printing heading.
-	print("»" + "—" * 60 + "»")                      # Creating top border.
-	cipher_list = encoding(text, open_key)           # Encoding text with open key.
-	i = 0                                            # Creating variable for control amount cols in row.
-	for item in cipher_list:                         # Passing through in cipher_list list.
-		if i == 7:                                   # Checking amount cols in row.
-			i = 0                                    # Resetting a variable.
-			print("\n")                              # Going to the next line.
-		print(" " + "%-8s" % item, end = "")         # Printing item value within 8 tabs.
-		i += 1                                       # Increment i for passing.
-	print("\n", end = "")                            # Going to the next line.
-	print("»" + "—" * 60 + "»")                      # Creating bottom border.
+def encoding_mode():
+	global text, open_key, cipher_list
+	import timeit
+	print("\n » Encoding mode activated! « (｡◕‿◕｡)")
+	try:
+		text = str(input("[+] Enter your text - "))
+		check = text[0]
+	except:
+		print("[x] Invalid input!")
+		raise SystemExit
+	print(" • 0. Random open key.\n • 1. Your open key(for experienced).")
+	key = int(input("[?] Select open key method - "))
+	if key == 0:
+		open_key, close_key = creation_keys()
+		print("[~] Your open key - [{},{}].".format(open_key[0], open_key[1]))
+		print("[~] Your close key - [{},{}].".format(close_key[0], close_key[1]))
+	elif key == 1:
+		open_key = tuple(map(int, str(input("[+] Enter your open key - ")).split(",")))
+	encoding_performance = "%.5f" % timeit.timeit(encoding, number = 1)
+	print("\n »» The result of encoding by RSA algorithm. ««")
+	print(cipher_list)
+	print("\n[~] RSA algorithm encoding performance - {} sec.".format(encoding_performance))
 
-def run_decode():
-	"""Function for run decoding."""
-	global cipher_list, close_key                      # Identify global variables.
-	print("\n RSA algorithm decoding result showing.") # Printing heading.
-	text = decoding(cipher_list, close_key)            # Decoding cipher list with close key.
-	print("«" + "—" * 60 + "«")                        # Creating top border.
-	print(" ", end = "")                               # Creating tabulation in beginning line.
-	i = 0                                              # Creating variable for control amount letters in line.
-	for letter in text:                                # Passing through in text.
-		if i == 60:                                    # Checking amount letters in line.
-			print("\n")                                # Going to the next line.
-			print(" ", end = "")                       # Creating tabulation in beginning line.
-		print(letter, end = "")                        # Printing letter.
-		i += 1                                         # Increment i for passing.
-	print("\n", end = "")                              # Going to the next line.
-	print("«" + "—" * 60 + "«\n")                      # Creating bottom border.
+def decoding_mode():
+	global text, close_key, cipher_list
+	import timeit
+	print("\n » Decoding mode activated! « (~˘▾˘)~")
+	try:
+		cipher_list = input("[+] Enter your cipher list - ")
+		cipher_list = list(map(int, cipher_list[1:len(cipher_list) - 1].split(",")))
+	except:
+		print("[x] Invalid input!")
+		raise SystemExit
+	close_key = tuple(map(int, str(input("[+] Enter your close key - ")).split(",")))
+	decoding_performance = "%.5f" % timeit.timeit(decoding, number = 1)
+	print("\n »» The result of decoding by RSA algorithm. ««")
+	print(text)
+	print("\n[~] RSA algorithm decoding performance - {} sec.".format(decoding_performance))
 
-def performance():
-	"""Function for counting and printing performance."""
-	from timeit import timeit                                             # Importing timeit item from timeit module.
-	elapsed_time_encode = "%.5f" % timeit(run_encode, number = 1)         # Counting time spent on encoding.
-	elapsed_time_decode = "%.5f" % timeit(run_decode, number = 1)         # Counting time spent on decoding.
-	print(" RSA algorithm performance statistics.")                       # Printing heading.
-	print("»" + "—" * 60 + "»")                                           # Creating top border.
-	print(" Time spent on encoding: {} sec.".format(elapsed_time_encode)) # Printing time spent on encoding.
-	print(" Time spent on decoding: {} sec.".format(elapsed_time_decode)) # Printing time spent on decoding.
-	print("»" + "—" * 60 + "»\n")                                         # Creating bottom border.
+def menu():
+	print("\t\t✖ Rivest–Shamir–Adleman cryptography algorithm. ✖")
+	print(" • 0. Demonstration mode.\n • 1. Encoding mode.\n • 2. Decoding mode.")
+	try:
+		id = int(input("[?] Select program mode - "))
+	except:
+		print("[x] Invalid input!")
+		raise SystemExit
+	if id == 0:
+		demonstration_mode()
+	elif id == 1:
+		encoding_mode()
+	elif id == 2:
+		decoding_mode()
 
-print("\t\tRivest–Shamir–Adleman cryptography algorithm.") # Printing heading.
-text, open_key, close_key = user_input()                   # Unpacking user input in global variables.
-cipher_list = []                                           # Creating cipher_list list for cipher values.
-performance()                                              # Calling performance function and starting algorithm.
+open_key, close_key, text, cipher_list = (), (), "", []
+menu()
