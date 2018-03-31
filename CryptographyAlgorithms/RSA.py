ff711cd8a6aeaf296d972dce1dc451707fa9de54
InfSecEng <ilyas.salimov.07@gmail.com>
2018-03-31 21:14:20 +0500

    added comments

--- CryptographyAlgorithms/RSA.py
+++ CryptographyAlgorithms/RSA.py
@@ -29,15 +29,15 @@ def creation_keys():
 
 def encoding():
 	"""Functionn for encoding text with RSA algorithm."""
-	global text, open_key, cipher_list
+	global text, open_key, cipher_list           # Identify global variables.
 	cipher_list = []                             # Creating cipher_list list for encoded chars.
 	e, n = open_key                              # Unpacking open_key tuple in e and n variables.
 	for letter in text:                          # Passing through from text.
 		cipher_list.append(ord(letter) ** e % n) # Append encoded char in cipher_list.
 
 def decoding():
-	"""Functionn for decoding text with RSA algorithm."""
-	global cipher_list, close_key, text
+	"""Function for decoding text with RSA algorithm."""
+	global cipher_list, close_key, text # Identify global variables.
 	text = ""                           # Creating text string variable for decoded chars.
 	d, n = close_key                    # Unpacking close_key tuple in d and n variables.
 	for item in cipher_list:            # Passing through from cipher_list.
@@ -48,81 +48,87 @@ def decoding():
 # ============================
 
 def demonstration_mode():
-	global text, open_key, close_key, cipher_list
-	import timeit
-	print("\n » Demonstration mode activated! « ¯\_(ツ)_/¯")
-	try:
-		text = str(input("[+] Enter your text - "))
-		check = text[0]
-	except:
-		print("[x] Invalid input!")
-		raise SystemExit
-	open_key, close_key = creation_keys()
-	print("[~] Your open key - [{},{}].".format(open_key[0], open_key[1]))
-	print("[~] Your close key - [{},{}].".format(close_key[0], close_key[1]))
-	encoding_performance = "%.5f" % timeit.timeit(encoding, number = 1)
-	print("\n »» The result of encoding by RSA algorithm. ««")
-	print(cipher_list)
-	text = ""
-	decoding_performance = "%.5f" % timeit.timeit(decoding, number = 1)
-	print("\n »» The result of decoding by RSA algorithm. ««")
-	print(text)
-	print("\n[~] RSA algorithm encoding performance - {} sec.".format(encoding_performance))
-	print("[~] RSA algorithm decoding performance - {} sec.".format(decoding_performance))
+	"""Function for working demonstration mode."""
+	global text, open_key, close_key, cipher_list                                            # Identify global variables.
+	import timeit                                                                            # Importing timeit module.
+	print("\n » Demonstration mode activated! « ¯\_(ツ)_/¯")                                 # Pringing header.
+	try:                                                                                     # Exception construction start.
+		text = str(input("[+] Enter your text - "))                                          # Input text.
+		check = text[0]                                                                      # Checking for emptiness.
+	except:                                                                                  # Exception handling.
+		print("[x] Invalid input! For example: \"hello\".")                                  # Printing information to user.
+		raise SystemExit                                                                     # Exit program.
+	open_key, close_key = creation_keys()                                                    # Creating open and close keys.
+	print("[~] Your open key - [{},{}].".format(open_key[0], open_key[1]))                   # Printing open key.
+	print("[~] Your close key - [{},{}].".format(close_key[0], close_key[1]))                # Printing close key.
+	encoding_performance = "%.5f" % timeit.timeit(encoding, number = 1)                      # Counting encoding elapse time.
+	print("\n »» The result of encoding by RSA algorithm. ««")                               # Printing encoding result header.
+	print(cipher_list)                                                                       # Printing encoding result.
+	text = ""                                                                                # Cleaning text variable.
+	decoding_performance = "%.5f" % timeit.timeit(decoding, number = 1)                      # Counting decoding elapse time.
+	print("\n »» The result of decoding by RSA algorithm. ««")                               # Printung decoding result header.
+	print(text)                                                                              # Printing decoding result.
+	print("\n[~] RSA algorithm encoding performance - {} sec.".format(encoding_performance)) # Printing encoding performance.
+	print("[~] RSA algorithm decoding performance - {} sec.".format(decoding_performance))   # Printing decoding performance.
 
 def encoding_mode():
-	global text, open_key, cipher_list
-	import timeit
-	print("\n » Encoding mode activated! « (｡◕‿◕｡)")
-	try:
-		text = str(input("[+] Enter your text - "))
-		check = text[0]
-	except:
-		print("[x] Invalid input!")
-		raise SystemExit
-	print(" • 0. Random open key.\n • 1. Your open key(for experienced).")
-	key = int(input("[?] Select open key method - "))
-	if key == 0:
-		open_key, close_key = creation_keys()
-		print("[~] Your open key - [{},{}].".format(open_key[0], open_key[1]))
-		print("[~] Your close key - [{},{}].".format(close_key[0], close_key[1]))
-	elif key == 1:
-		open_key = tuple(map(int, str(input("[+] Enter your open key - ")).split(",")))
-	encoding_performance = "%.5f" % timeit.timeit(encoding, number = 1)
-	print("\n »» The result of encoding by RSA algorithm. ««")
-	print(cipher_list)
-	print("\n[~] RSA algorithm encoding performance - {} sec.".format(encoding_performance))
+	"""Function for working encoding mode."""
+	global text, open_key, cipher_list                                                       # Identify global variables.
+	import timeit                                                                            # Importing timeit module.
+	print("\n » Encoding mode activated! « (｡◕‿◕｡)")                                         # Printing header.
+	try:                                                                                     # Exception construction start.
+		text = str(input("[+] Enter your text - "))                                          # Input text.
+		check = text[0]                                                                      # Checking for emptiness.
+	except:                                                                                  # Exception handling.
+		print("[x] Invalid input! For example: \"hello\".")                                  # Printing information to user.
+		raise SystemExit                                                                     # Exit program.
+	print(" • 0. Random open key.\n • 1. Your open key(for experienced).")                   # Print selection menu.
+	key = int(input("[?] Select open key method - "))                                        # Input user choice.
+	if key == 0:                                                                             # If user choiced random key then 
+		open_key, close_key = creation_keys()                                                # - creating open and close keys,
+		print("[~] Your open key - [{},{}].".format(open_key[0], open_key[1]))               # - printing open key,
+		print("[~] Your close key - [{},{}].".format(close_key[0], close_key[1]))            # - printing close key.
+	elif key == 1:                                                                           # Else if user choiced his key then
+		open_key = tuple(map(int, str(input("[+] Enter your open key - ")).split(",")))      # - input user open key.
+	encoding_performance = "%.5f" % timeit.timeit(encoding, number = 1)                      # Counting encoding elapse time.
+	print("\n »» The result of encoding by RSA algorithm. ««")                               # Printing encoding result header.
+	print(cipher_list)                                                                       # Printing encoding result.
+	print("\n[~] RSA algorithm encoding performance - {} sec.".format(encoding_performance)) # Printing encoding performance.
 
 def decoding_mode():
-	global text, close_key, cipher_list
-	import timeit
-	print("\n » Decoding mode activated! « (~˘▾˘)~")
-	try:
-		cipher_list = input("[+] Enter your cipher list - ")
-		cipher_list = list(map(int, cipher_list[1:len(cipher_list) - 1].split(",")))
-	except:
-		print("[x] Invalid input!")
-		raise SystemExit
-	close_key = tuple(map(int, str(input("[+] Enter your close key - ")).split(",")))
-	decoding_performance = "%.5f" % timeit.timeit(decoding, number = 1)
-	print("\n »» The result of decoding by RSA algorithm. ««")
-	print(text)
-	print("\n[~] RSA algorithm decoding performance - {} sec.".format(decoding_performance))
+	"""Function for working decoding mode."""
+	global text, close_key, cipher_list                                                      # Identify global variables.
+	import timeit                 															 # Importing timeit module.
+	print("\n » Decoding mode activated! « (~˘▾˘)~")                                         # Printing header.
+	try:																					 # Exception construction start.
+		cipher_list = input("[+] Enter your cipher list - ")                                 # Input string.
+		cipher_list = list(map(int, cipher_list[1:len(cipher_list) - 1].split(",")))         # Creating list of string and passing through for use int() for every item.
+	except: 																				 # Exception handling.
+		print("[x] Invalid input! For example: \"[1, 2, 3, 4, 5]\".")                        # Printing information to user.
+		raise SystemExit																     # Exit program.
+	close_key = tuple(map(int, str(input("[+] Enter your close key - ")).split(",")))        # Input user close key.
+	decoding_performance = "%.5f" % timeit.timeit(decoding, number = 1)                      # Counting decoding elapse time.
+	print("\n »» The result of decoding by RSA algorithm. ««") 								 # Printing decoding result header.
+	print(text) 																			 # Printing decoding result.
+	print("\n[~] RSA algorithm decoding performance - {} sec.".format(decoding_performance)) # Printing decoding performance.
 
 def menu():
-	print("\t\t✖ Rivest–Shamir–Adleman cryptography algorithm. ✖")
-	print(" • 0. Demonstration mode.\n • 1. Encoding mode.\n • 2. Decoding mode.")
-	try:
-		id = int(input("[?] Select program mode - "))
-	except:
-		print("[x] Invalid input!")
-		raise SystemExit
-	if id == 0:
-		demonstration_mode()
-	elif id == 1:
-		encoding_mode()
-	elif id == 2:
-		decoding_mode()
+	print("\t\t✖ Rivest–Shamir–Adleman cryptography algorithm. ✖")                 # Printing header.
+	print(" • 0. Demonstration mode.\n • 1. Encoding mode.\n • 2. Decoding mode.") # Print selection menu.
+	try:                                                                           # Exception construction start.
+		id = int(input("[?] Select program mode - "))                              # Input user choice.
+	except:                                                                        # Exception handling.
+		print("[x] Invalid input! For example: \"0, 1, 2\".")                      # Printing information to user.
+		raise SystemExit                                                           # Exit program.
+	if id == 0:                                                                    # If user choiced demonstation mode then
+		demonstration_mode() 													   # - calling demonstration_mode().
+	elif id == 1: 																   # Else if user choiced encoding mode then
+		encoding_mode() 														   # - calling encoding_mode().
+	elif id == 2:																   # Else if user choiced decoding mode then 
+		decoding_mode() 														   # - calling decoding_mode().
+	else:                                                                          # Else then 
+		print("[x] Invalid input! You can only select: \"0, 1, 2\".")              # - printing information to user.
+		raise SystemExit                                                           # - exit program.
 
-open_key, close_key, text, cipher_list = (), (), "", []
-menu()
+open_key, close_key, text, cipher_list = (), (), "", [] # Creating global variables.
+menu() 													# Starting program.
