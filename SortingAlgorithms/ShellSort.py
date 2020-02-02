f081ccb7be5c0a2271d1f00c2d115d7e91e22f0f
fsciety <ilyas.salimov.07@gmail.com>
2020-02-02 12:55:01 +0300

    refactoring & fixed mistakes

--- SortingAlgorithms/ShellSort.py
+++ SortingAlgorithms/ShellSort.py
@@ -1,39 +1,44 @@
-def shellSort(collection):
-	lenght = len(collection)
-	middle, counter = lenght // 2, 0
+from random import randint
 
-	while middle > 0:
-		for i in range(0, lenght - middle):
-			j = i
-			while (j >= 0) and (collection[j] > collection[j + middle]):
-				temp = collection[j]
-				collection[j] = collection[j + middle]
-				collection[j + middle] = temp
 
-				j, counter = j - 1, counter + 1
-				print(" ", [counter], "-->", collection)
+def shell_sort(collection):
+    length = len(collection)
+    middle, counter = length // 2, 0
 
-		middle = middle // 2
+    while middle > 0:
+        for i in range(0, length - middle):
+            j = i
+            while (j >= 0) and (collection[j] > collection[j + middle]):
+                temp = collection[j]
+                collection[j] = collection[j + middle]
+                collection[j + middle] = temp
 
-	return collection, counter
+                j, counter = j - 1, counter + 1
+                print("Step %i -->" % counter, collection)
+
+        middle = middle // 2
+
+    return collection, counter
+
 
 def visualization():
-	from random import randint
-	lenght = 10
-	collection = [randint(0, lenght) for i in range(lenght)]
+    length = 10
+    collection = [randint(0, length) for _ in range(length)]
 
-	print("Initial list:", collection)
-	print("Visualization of algorithm work.")
+    print("Initial list:", collection)
+    print("Visualization of algorithm work.")
 
-	collection, counter = shellSort(collection)
+    collection, counter = shell_sort(collection)
 
-	print("Final list:", collection)
-	print("Total numbers of passages:", counter)
+    print("Final list:", collection)
+    print("Total numbers of passages:", counter)
+
 
 def main():
-	import timeit
-	elapsedTime = timeit.timeit(visualization, number = 1)
-	print("Elapsed time: ", round(elapsedTime, 3), "sec.")
+    import timeit
+    elapsed_time = timeit.timeit(visualization, number=1)
+    print("Elapsed time: ", round(elapsed_time, 7), "sec.")
+
 
 if __name__ == '__main__':
-	main()
+    main()
